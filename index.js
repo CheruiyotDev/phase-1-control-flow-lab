b4d390a989ed50fc1767d68c71acd58df4dab4da
Cheruiyot Evans <chersoftwaredev@gmail.com>
2024-06-27 17:29:32 +0300

    Control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride,chargeInDollars){
   // Write your code here!
+  
+  if (ride <= 400) {
+    return "This one is on me!";
+  } else if (ride >= 400 && ride < 2000) {
+    return "That will be twenty bucks.";
+  } else if (ride >= 2000 && ride < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (ride > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  // Write your code here
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
+  
 }
+console.log(ternaryCheckCity())
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  // if (tip === "generous") {
+  //   return "Thank you so much.";
+  // } else if (tip === "not as generous") {
+  //   return "Thank you.";
+  // } else {
+  //   return "Bye.";
+  // }
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+      break;
+  }
 }
